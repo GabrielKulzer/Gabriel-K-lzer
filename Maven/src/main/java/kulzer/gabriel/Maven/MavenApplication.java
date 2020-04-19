a079526b4dde512bd91740ea32eea5c298d228a8
GabrielKulzer <63792782+GabrielKulzer@users.noreply.github.com>
2020-04-19 18:45:51 -0300

    Add files via upload

---
+++ Maven/src/main/java/kulzer/gabriel/Maven/MavenApplication.java
@@ -0,0 +1,18 @@
+package kulzer.gabriel.Maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**@Author - Gabriel Külzer
+ * @Since /17/04/2020
+ * @Version - 1.0
+ */
+
+@SpringBootApplication
+public class MavenApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+		System.out.println("asasasasasasasasa");
+
+	}
+}
