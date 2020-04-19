5b991310645c2d4588a26330084822f27614c156
GabrielKulzer <63792782+GabrielKulzer@users.noreply.github.com>
2020-04-19 18:46:47 -0300

    Add files via upload

---
+++ Gradle com Rest repositories/src/main/java/hello/world/gradle/HelloWorldGradleApplication.java
@@ -0,0 +1,16 @@
+package hello.world.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class HelloWorldGradleApplication {
+
+	public static void main(String[] args)
+	{
+		ApplicationContext applicationContext = SpringApplication.run(HelloWorldGradleApplication.class, args);
+		System.out.println("Hello World!!");
+	}
+
+}
---
+++ Gradle com Rest repositories/src/test/java/hello/world/gradle/HelloWorldGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.world.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
